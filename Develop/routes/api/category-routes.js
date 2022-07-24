399389bdcee1e3b761c4f7af109d65370717e38f
Tazaev <tonymurtazaev@gmail.com>
2022-07-23 22:10:29 -0400

    added routes to category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryInfo = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryInfo = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryInfo) {
+      res.status(404).json({ message: "No product found" });
+      return;
+    }
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
