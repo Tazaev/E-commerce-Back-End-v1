3fb0e9e7c01e35777c2e50867bb5433b7ad77e94
Tazaev <tonymurtazaev@gmail.com>
2022-07-23 22:16:26 -0400

    added routes in tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,15 +6,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagInfo) {
+      res.status(404).json({ message: "No tag Id found" });
+      return;
+    }
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  
 });
 
 router.put('/:id', (req, res) => {
